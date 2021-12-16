f1b805e4dcc2e942a57cf0edbdd26021aad39270
Daniel Lawrence <dan.lawrence0810@gmail.com>
2021-12-16 20:55:35 +1030

    basic JS added to give comments on what is wanted

--- script.js
+++ script.js
@@ -1,4 +1,32 @@
-alert('Let\'s play rock, paper, scissors')
+//Game where user plays rock, paper, scissor against computer
+//variable of user and computer
+// R P & S in array with index number
+//function has input for r, p or s
+//computer randomnly selects option after user input on prompt screen
+// need prompt for user to select followed by alert with computer selection followed by alert with result
+//then need alert to show stats
+//then need confirm to see if play a second round
 
-confirm('Are you ready to play?')
 
+//if user put R and computer put S user wins, if comp selects P comp wins and if comp selects R its a tie
+//if user puts P and comp puts R user wins, if comp selects S comp wins and if comp selects P its a tie
+//if user puts S and comp puts P user wins, if comp selects R comp wins and if comp selects S its a tie
+
+//array for options
+let options = ['R', 'P', 'S'];
+
+//variables for user and computer 
+let player1 = 'User'
+let player2 = 'Computer'
+
+//prompt for user selection
+prompt("Please select R, P or S")
+
+//alert for computer selection
+window.alert('Computer has selected ')
+
+//alert for stats 
+window.alert()
+
+//confirm if want to play again
+window.confirm('Would you like to play again?')
