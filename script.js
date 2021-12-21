d93324dd552b68b939c4bfcaf9ed707efa6d20e3
Daniel Lawrence <dan.lawrence0810@gmail.com>
2021-12-21 14:02:23 +1030

    set up initial variables

--- script.js
+++ script.js
@@ -7,48 +7,7 @@
 //then need alert to show stats
 //then need confirm to see if play a second round
 
-let wins = 0
-let losses = 0
-let ties = 0
-//array for options
-let options = ['R', 'P', 'S'];
+var wins = 0;
+var losses = 0;
+var ties = 0;
 
-var match = function (){
-    var userOption = prompt('Enter R, P or S')
-
-    if(!userOption){
-        return;
-    }
-
-    userOption = userOption.toUpperCase();
-
-    var index = Math.floor(Math.random() * options.length);
-    var compOption = options[index];
-
-    alert('The computer chose ' + compOption)
-
-    if (userOption === compOption){
-        ties++;
-        alert('This is a tie');
-    } else if (
-        (userOption === 'R' && compOption === 'S') ||
-        (userOption === 'S' && compOption === 'P') ||
-        (userOption === 'P' && compOption === 'R')
-    ) {
-        wins++;
-        alert('You have won!')
-    } else {
-        losses++;
-        alert('You have lost!')
-    }
-
-    alert('Stats: \nWins = ' + wins + '\nLosses = ' + losses + '\nTies = ' + ties)
-
-    var nextRound = confirm('Do you want to play another game?')
-
-    if (nextRound){
-        match()
-    }
-}
-
-match()
