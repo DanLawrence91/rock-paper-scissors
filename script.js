e6af48a363241da9e9654d8da5b129d935892996
Daniel Lawrence <dan.lawrence0810@gmail.com>
2021-12-21 14:18:55 +1030

    function for game added

--- script.js
+++ script.js
@@ -7,7 +7,57 @@
 //then need alert to show stats
 //then need confirm to see if play a second round
 
+//stats for wins, losses and ties
 var wins = 0;
 var losses = 0;
 var ties = 0;
 
+//options to be chosen from
+var options = ['R', 'P', 'S'];
+
+//function for game of rock paper scissors to be played
+var rPS = function(){
+    //user chooses from initial prompt
+    var userChoice = prompt("Please choose either R, P or S");
+
+    //convert to uppercase if chosen in lower
+    userChoice = userChoice.toUpperCase();
+
+    //if no choice made game ends
+    if (!userChoice){
+        return;
+    }
+
+    //randomly choose computer option
+    var compIndex = Math.floor(Math.random() * options.length);
+    var compChoice = options[compIndex];
+
+    alert("The computer has chosen " + compChoice);
+
+    //results returned depending on what selections have been made by user and computer
+    if ((userChoice === "R" && compChoice === "S") || (userChoice === "p" && compChoice === "R") || (userChoice === "R" && compChoice === "S")) { 
+        wins++;
+        alert("You have won this game");
+    } else if (userChoice === compChoice){
+        ties++;
+        alert("The game is a tie")
+    } else {
+        losses++
+        alert("The computer has won")
+    }
+
+    //log stats for games played
+    alert("Stats: Wins = " + wins + "\nLosses = " + losses + "\n Ties = " + ties);
+
+    //ask user if they want to play again
+    confirm("Do you want to play again?");
+
+    //if confirm is true game restarts
+    if (confirm){
+        rPS()
+    };
+        
+}
+
+//call function to start game
+rPS()
